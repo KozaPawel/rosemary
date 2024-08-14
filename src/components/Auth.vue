7955b9c87ded8f0236c5d53ee52d40dfadbac1dc
KozaPawel <p.koza99@o2.pl>
2024-08-15 00:46:50 +0200

    Fix auth overflowing

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -9,11 +9,11 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="flex h-screen w-screen flex-row">
+  <div class="flex h-screen w-screen flex-row items-start md:items-center">
     <div class="hidden h-full w-1/2 md:block lg:w-2/3">
       <img src="@/assets/auth-bg.webp" class="pointer-events-none h-full w-full object-cover" />
     </div>
-    <div class="mt-24 flex h-full w-full flex-col p-4 md:mt-0 md:w-1/2 md:justify-center lg:w-1/3">
+    <div class="mt-24 flex h-fit w-full flex-col p-4 md:mt-0 md:w-1/2 md:justify-center lg:w-1/3">
       <slot></slot>
       <div class="mt-2 w-fit hover:cursor-pointer">
         <text @click="router.push('signup')" v-if="props.isLoginForm" class="group font-medium"
