5beee5c33b15f1f21dd1eed045e614420367e130
KozaPawel <p.koza99@o2.pl>
2024-08-15 16:23:56 +0200

    Add font and fix pagination on mobile

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const route = useRoute();
 </script>
 
 <template>
-  <div class="h-svh w-screen overflow-auto bg-light-background text-light-text">
+  <div class="h-dvh w-screen overflow-auto bg-light-background text-light-text">
     <Navbar class="sticky top-0 bg-light-background" v-if="route.meta.auth" />
     <RouterView />
   </div>
