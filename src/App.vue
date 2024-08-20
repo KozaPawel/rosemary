0c97936e4e2b4b2050ce1d0600e645ce43915e9a
KozaPawel <p.koza99@o2.pl>
2024-08-20 12:42:06 +0200

    Fix toast on mobile

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ const unmountUserView = () => {
 <template>
   <div class="h-dvh w-screen overflow-auto bg-light-background text-light-text">
     <Navbar class="sticky top-0 bg-light-background" v-if="route.meta.auth" />
-    <Toast />
+    <Toast :pt="{ root: { class: '!w-full sm:!w-96 pl-10 sm:pl-0 ' } }" />
     <RouterView v-slot="{ Component }">
       <KeepAlive :include="'UserView'" :exclude="unmountUserView()">
         <component :is="Component" />
