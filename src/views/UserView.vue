02ba46b839c5249946e6f1bb9ab4f191ac0e0cdf
KozaPawel <p.koza99@o2.pl>
2024-08-20 17:41:40 +0200

    Hide searchbar when no recipes

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -110,7 +110,11 @@ onBeforeMount(async () => {
           Add new recipe
         </button>
       </RouterLink>
-      <form @submit.prevent="filterRecipes()" class="mb-2 flex flex-wrap items-end gap-2">
+      <form
+        v-if="recipes.length > 0"
+        @submit.prevent="filterRecipes()"
+        class="mb-2 flex flex-wrap items-end gap-2"
+      >
         <LabeledInput
           v-model="searchQuery"
           :id="'search'"
